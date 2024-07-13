0d0c0deb0ca589840e6d51c21ce9ff0b5fec7a0b
Gelo <gelogelofi@gmail.com>
2024-07-13 16:12:14 +0800

    sidebar addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import Hero from './sections/Hero/Hero';
 import Projects from './sections/Hero/Projects/Projects';
+import Sidebar from './sections/Hero/Sidebar';
 
 function App() {
   return <>
+    <Sidebar/>
     <Hero/>
     <Projects/>
     </>
---
+++ src/sections/Hero/Sidebar.jsx
@@ -0,0 +1,18 @@
+import styles from "../Hero/Sidebar.module.css";
+
+function Sidebar() {
+    const noSidebar = window.matchMedia('(max-width: 768px').matches;
+    if(noSidebar){
+        return null;
+    } else {
+    return (
+        <div class={styles.sidenav}>
+            <a href="#hero">About</a>
+            <a href="#projects">Projects</a>
+            <a href="#designs">Designs</a>
+        </div>
+        )
+    }
+}
+
+export default Sidebar
